a2f9f295383ece88ccd4eee5ed3085ce52a15094
Gray SuitCase <ubatamamoon@gmail.com>
2021-03-24 21:35:21 +0900

    多重配列を追加

--- parse.c
+++ parse.c
@@ -198,7 +198,7 @@ static Type *func_params(Token **rest, Token *tok, Type *ty) {
 
 // type-suffixをパースする
 // type-suffix = ("(" func-params
-//             | "[" num "]"
+//             | "[" num "]" type-suffix
 //             | ε
 static Type *type_suffix(Token **rest, Token *tok, Type *ty) {
     if (equal(tok, "("))
@@ -206,7 +206,8 @@ static Type *type_suffix(Token **rest, Token *tok, Type *ty) {
 
     if (equal(tok, "[")) {
         int sz = get_number(tok->next);
-        *rest = skip(tok->next->next, "]");
+        tok = skip(tok->next->next, "]");
+        ty = type_suffix(rest, tok, ty);
         return array_of(ty, sz);
     }
 
