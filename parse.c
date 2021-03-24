42f04e05f1d3d32e20ac16bf0739cc7aea629c47
Gray SuitCase <ubatamamoon@gmail.com>
2021-03-24 22:03:30 +0900

    []演算子を追加

--- parse.c
+++ parse.c
@@ -85,6 +85,7 @@ static Node *equality(Token **rest, Token *tok);
 static Node *relational(Token **rest, Token *tok);
 static Node *add(Token **rest, Token *tok);
 static Node *mul(Token **rest, Token *tok);
+static Node *postfix(Token **rest, Token *tok);
 static Node *unary(Token **rest, Token *tok);
 static Node *primary(Token **rest, Token *tok);
 
@@ -454,7 +455,8 @@ static Node *mul(Token **rest, Token *tok) {
 }
 
 // unaryをパースする
-// unary = ("+" | "-" | "*" | "&")? unary | primary
+// unary = ("+" | "-" | "*" | "&")? unary
+//       | postfix
 static Node *unary(Token **rest, Token *tok) {
     if (equal(tok, "+"))
         return unary(rest, tok->next);
@@ -464,7 +466,24 @@ static Node *unary(Token **rest, Token *tok) {
         return new_unary(ND_ADDR, unary(rest, tok->next), tok);
     if (equal(tok, "*"))
         return new_unary(ND_DEREF, unary(rest, tok->next), tok);
-    return primary(rest, tok);
+    return postfix(rest, tok);
+}
+
+// postfixをパースする
+// postfix = primary ("[" expr "]")*
+static Node *postfix(Token **rest, Token *tok) {
+    Node *node = primary(&tok, tok);
+
+    while (equal(tok, "[")) {
+        // x[y] は *(x+y) と同じ意味
+        Token *start = tok;
+        Node *idx = expr(&tok, tok->next);
+        tok = skip(tok, "]");
+        node = new_unary(ND_DEREF, new_add(node, idx, start), start);
+    }
+
+    *rest = tok;
+    return node;
 }
 
 // funcallをパースする
