52899a87d224dd2d2cb3d5be48a8e32ce9d81d2a
Gray Suitcase <ubatamamoon@gmail.com>
2021-04-22 16:36:00 +0900

    -o と --help オプションを追加

--- main.c
+++ main.c
@@ -1,18 +1,62 @@
 #include "chibicc.h"
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません\n");
-        return 1;
+static char *opt_o;
+
+static char *input_path;
+
+static void usage(int status) {
+    fprintf(stderr, "chibicc [ -o <path> ] <file>\n");
+    exit(status);
+}
+
+static void parse_args(int argc, char **argv) {
+    for (int i = 0; i < argc; i++) {
+        if (!strcmp(argv[i], "--help"))
+            usage(0);
+
+        if (!strcmp(argv[i], "-o")) {
+            if (!argv[++i])
+                usage(1);
+            opt_o = argv[i];
+            continue;
+        }
+
+        if (!strncmp(argv[i], "-o", 2)) {
+            opt_o = argv[i] + 2;
+            continue;
+        }
+
+        if (argv[i][0] == '-' && argv[i][1] != '\0')
+            error("不正な引数です: %s", argv[i]);
+
+        input_path = argv[i];
     }
 
+    if (!input_path)
+        error("入力元ファイルがありません");
+}
+
+static FILE *open_file(char *path) {
+    if (!path || strcmp(path, "-") == 0)
+        return stdout;
+
+    FILE *out = fopen(path, "w");
+    if (!out)
+        error("出力先ファイルを開けませんでした: %s: %s", path, strerror(errno));
+    return out;
+}
+
+int main(int argc, char **argv) {
+    parse_args(argc, argv);
+
     // トークナイズする
-    Token *tok = tokenize_file(argv[1]);
+    Token *tok = tokenize_file(input_path);
 
     // パースする
     Obj *prog = parse(tok);
 
     // ASTを走査してアセンブリを出力する
-    codegen(prog);
+    FILE *out = open_file(opt_o);
+    codegen(prog, out);
     return 0;
 }
