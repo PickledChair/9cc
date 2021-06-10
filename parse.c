313d624e3220365207e2dcdc145ff7f986c12835
Gray SuitCase <ubatamamoon@gmail.com>
2021-06-10 12:21:01 +0900

    void型を追加

--- parse.c
+++ parse.c
@@ -190,8 +190,14 @@ static Node *primary(Token **rest, Token *tok);
 
 // 与えられたトークンが型を表している場合、trueを返す
 static bool is_typename(Token *tok) {
-    return equal(tok, "char") || equal(tok, "short") || equal(tok, "int") ||
-            equal(tok, "long") || equal(tok, "struct") || equal(tok, "union");
+    static char *kw[] = {
+        "void", "char", "short", "int", "long", "struct", "union"
+    };
+
+    for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
+        if (equal(tok, kw[i]))
+            return true;
+    return false;
 }
 
 // stmtをパースする
@@ -289,8 +295,14 @@ static void push_tag_scope(Token *tok, Type *ty) {
 }
 
 // declspecをパースする
-// declspec = "char" | "short" | "int" | "long" | struct-decl | union-decl
+// declspec = "void" | "char" | "short" | "int" | "long"
+//           | struct-decl | union-decl
 static Type *declspec(Token **rest, Token *tok) {
+    if (equal(tok, "void")) {
+        *rest = tok->next;
+        return ty_void;
+    }
+
     if (equal(tok, "char")) {
         *rest = tok->next;
         return ty_char;
@@ -399,6 +411,9 @@ static Node *declaration(Token **rest, Token *tok) {
             tok = skip(tok, ",");
 
         Type *ty = declarator(&tok, tok, basety);
+        if (ty->kind == TY_VOID)
+            error_tok(tok, "void 型の変数を宣言しています");
+
         Obj *var = new_lvar(get_ident(ty->name), ty);
 
         if (!equal(tok, "="))
